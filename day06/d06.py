9c782c1a34ffd741e3518253e8983ef2611b603d
Hai-Nam Nguyen <han.nguyen@criteo.com>
2023-12-06 06:26:15 +0100

    Day 06 Part 2 proper solution

--- day06/d06.py
+++ day06/d06.py
@@ -24,22 +24,26 @@ class Problem:
         return p
 
     def solve2(self):
-        return 0
+        return self.solve()
 
 
 class Solver:
     def __init__(self, input) -> None:
-        self.input = {
-            'time': list(map(int, input[0].split(': ')[1].split())),
-            'distance': list(map(int, input[1].split(': ')[1].split())),
-        }
+        self.input = input
 
     def solve(self, part=1):
-        problem = Problem(self.input)
+        # Take the time to properly parse the input for part 2. In real life,
+        # I searched and replaced all spaces then run part 1 again for part 2.
+        t = self.input[0].split(': ')[1]
+        d = self.input[1].split(': ')[1]
+        problem = Problem({
+            'time': list(map(int, (t if part == 1 else t.replace(' ', '')).split())),
+            'distance': list(map(int, (d if part == 1 else d.replace(' ', '')).split())),
+        })
         return problem.solve() if part==1 else problem.solve2()
 
 
 f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip().split('\n'))
 print("Puzzle 1: ", solver.solve())
-#print("Puzzle 2: ", solver.solve(2))
+print("Puzzle 2: ", solver.solve(2))

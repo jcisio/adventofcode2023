10f4a5f4ff292c9da005ffa54f991729fd01dd69
Hai-Nam Nguyen <han.nguyen@criteo.com>
2023-12-09 06:30:19 +0100

    Day 09 Part 2

--- day09/d09.py
+++ day09/d09.py
@@ -4,15 +4,13 @@ Advent Of Code
 https://adventofcode.com/2023/day/9
 """
 from __future__ import annotations
-from collections import defaultdict
-import parse
 
 
 class Problem:
     def __init__(self, input) -> None:
         self.input = input
 
-    def extrapolate(self, seq):
+    def extrapolate(self, seq, part=1):
         seqs = []
         seqs.append(seq)
         seq1 = seq.copy()
@@ -23,15 +21,18 @@ class Problem:
 
         seqs[-1].append(0)
         for i in range(len(seqs)-1, 0, -1):
-            seqs[i-1].append(seqs[i][-1] + seqs[i-1][-1])
-#        print(seqs[0][-1])
-        return seqs[0][-1]
+            if part == 1:
+                seqs[i-1].append(seqs[i][-1] + seqs[i-1][-1])
+            else:
+                seqs[i-1].insert(0, seqs[i-1][0] - seqs[i][0])
+            #print(seqs[i-1])
+        return seqs[0][-1 if part == 1 else 0]
 
     def solve(self):
         return sum([self.extrapolate(line) for line in self.input])
 
     def solve2(self):
-        return 0
+        return sum([self.extrapolate(line, part=2) for line in self.input])
 
 
 class Solver:
@@ -46,4 +47,4 @@ class Solver:
 f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip().split('\n'))
 print("Puzzle 1: ", solver.solve())
-#print("Puzzle 2: ", solver.solve(2))
+print("Puzzle 2: ", solver.solve(2))

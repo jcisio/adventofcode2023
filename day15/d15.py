29242e6a4af8915350b13e34982fc661a2c85d08
Hai-Nam Nguyen <han.nguyen@criteo.com>
2023-12-15 11:23:19 +0100

    Day 15 Part 2

--- day15/d15.py
+++ day15/d15.py
@@ -5,30 +5,48 @@ https://adventofcode.com/2023/day/15
 """
 from __future__ import annotations
 from collections import defaultdict
-import parse
+import re
 
 
 class Problem:
     def __init__(self, input) -> None:
         self.input = input
 
+    def hash(self, s):
+        h = 0
+        for c in s:
+            h = (h + ord(c)) * 17 % 256
+        return h
+
     def solve(self):
-        total = 0
-        for m in self.input.split(','):
-            s = 0
-            for c in m:
-                s = (s + ord(c)) * 17 % 256
-            total += s
-#            print(m, s)
-        return total
+        return sum(self.hash(m) for m in self.input)
 
     def solve2(self):
-        return 0
+        boxes = [[] for _ in range(256)]
+        for m in self.input:
+            label, op, focal = re.match(r'(\w+)([\-\=])(\d*)', m).groups()
+            h = self.hash(label)
+            if op == '-':
+                boxes[h] = [lens for lens in boxes[h] if lens[0] != label]
+            elif op == '=':
+                found = False
+                for lens in boxes[h]:
+                    if lens[0] == label:
+                        lens[1] = focal
+                        found = True
+                        break
+                if not found:
+                    boxes[h].append([label, focal])
+            s = 0
+            for i, box in enumerate(boxes):
+                for j, lens in enumerate(box):
+                    s += (i+1) * (j+1) * int(lens[1])
+        return s
 
 
 class Solver:
     def __init__(self, input) -> None:
-        self.input = input
+        self.input = input.split(',')
 
     def solve(self, part=1):
         problem = Problem(self.input)
@@ -38,4 +56,4 @@ class Solver:
 f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip())
 print("Puzzle 1: ", solver.solve())
-#print("Puzzle 2: ", solver.solve(2))
+print("Puzzle 2: ", solver.solve(2))

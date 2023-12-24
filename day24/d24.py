85ca904d8e454c456e5fbc2122e559938e6ea1ed
Hai-Nam Nguyen <han.nguyen@criteo.com>
2023-12-24 10:37:15 +0100

    Day 25 Part 2 WIP

--- day24/d24.py
+++ day24/d24.py
@@ -4,8 +4,9 @@ Advent Of Code
 https://adventofcode.com/2023/day/24
 """
 from __future__ import annotations
-from collections import defaultdict
 import parse
+import math
+from collections import Counter
 
 
 class Problem:
@@ -36,7 +37,54 @@ class Problem:
         #return self.count(7, 27)
         return self.count(200000000000000, 400000000000000)
 
+    def gcd(self, v, dim):
+        stones = [s for s in self.stones if s[3+dim] == v]
+        delta = []
+        for i in range(len(stones)-1):
+            for j in range(i+1, len(stones)):
+                delta.append(stones[j][dim]-stones[i][dim])
+        gcd = delta[0]
+        for i in range(1, len(delta)):
+            gcd = math.gcd(gcd, delta[i])
+        return gcd
+
+    def solve_dim(self, dim):
+        count = Counter()
+        for s in self.stones:
+            count[s[3+dim]] += 1
+        for c,_ in count.most_common(1):
+            stones = [s for s in self.stones if s[3+dim] == c]
+            delta = []
+            for i in range(len(stones)-1):
+                for j in range(i+1, len(stones)):
+                    delta.append(stones[j][dim]-stones[i][dim])
+            gcd = delta[0]
+            for i in range(1, len(delta)):
+                gcd = math.gcd(gcd, delta[i])
+            v = stones[0][3+dim] - gcd
+
+        stones = sorted(self.stones, key=lambda x: x[3+dim])
+        for s in stones: print(s)
+
+        # Same direction
+        s_right = [s[dim] for s in stones if s[3+dim] < v and s[3+dim] * v > 0]
+        s_left = [s[dim] for s in stones if s[3+dim] > v and s[3+dim] * v > 0]
+        s_min = max(s_left) if s_left else -1e20
+        s_max = min(s_right) if s_right else 1e20
+
+        # Inverse direction
+        if v < 0:
+            id_min = max([s[dim] for s in stones if s[3+dim] > 0])
+            s_min = min(s_min, id_min)
+        else:
+            id_min = min([s[dim] for s in stones if s[3+dim] < 0])
+            s_max = max(s_max, id_min)
+
+        print(s_min, s_max)
+
+
     def solve2(self):
+        self.solve_dim(0)
         return 0
 
 
@@ -51,5 +99,5 @@ class Solver:
 
 f = open(__file__[:-3] + '.test', 'r')
 solver = Solver(f.read().strip().split('\n'))
-print("Puzzle 1: ", solver.solve())
-#print("Puzzle 2: ", solver.solve(2))
+#print("Puzzle 1: ", solver.solve())
+print("Puzzle 2: ", solver.solve(2))
